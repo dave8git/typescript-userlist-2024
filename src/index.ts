096990b1d89a858edcb27356c66fdaf4cae85143
dave8git <dave8git@gmail.com>
2024-11-25 19:55:38 +0100

    add iquirer WIP

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+
+const startApp = () => {
+    inquirer.prompt([{
+        name: 'action',
+        type: 'input',
+        message: 'How can I help you?',
+      }]).then((answers: { action: 'list' | 'add' | 'remove' | 'quit' }) => {
+        console.log("Chosen action: " + answers.action);
+        startApp();
+        if (answers.action === "quit") 
+            return;
+      });
+}
+
+startApp();
